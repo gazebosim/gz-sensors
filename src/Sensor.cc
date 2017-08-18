6a65d7df6b8f2ec48ee72a60d3d01d26e1c731eb
Shane Loretz <sloretz@osrfoundation.org>
2017-08-18 09:13:44 -0700

    Load basic parameters from sdf

--- src/Sensor.cc
+++ src/Sensor.cc
@@ -15,22 +15,91 @@
  *
 */
 
-#include <ignition/sensors/Sensor.hh>
+#include "ignition/sensors/Sensor.hh"
+
+#include <vector>
 
 using namespace ignition::sensors;
 
 
+struct PluginDescription
+{
+  /// \brief Name of a plugin to load
+  public: std::string pluginName;
+
+  /// \brief Library name of a plugin to load
+  public: std::string pluginFileName;
+};
+
+
 class ignition::sensors::SensorPrivate
 {
+  /// \brief Populates fields from a <sensor> element
+  public: void PopulateFromSDF(sdf::ElementPtr _sdf);
+
   /// \brief id given to sensor when constructed
   public: SensorId id;
 
   /// \brief name given to sensor when loaded
   public: std::string name;
 
+  /// \brief topic to send sensor data
+  public: std::string topic;
+
   /// \brief Pose of the sensor
   public: ignition::math::Pose3d pose;
+
+  /// \brief How many times the sensor will generate data per second
+  public: double updateRate;
+
+  /// \brief plugins this sensor uses
+  std::vector<PluginDescription> plugins;
 };
+
+//////////////////////////////////////////////////
+void SensorPrivate::PopulateFromSDF(sdf::ElementPtr _sdf)
+{
+  // All SDF code gets auto converted to latest version. This code is
+  // written assuming sdformat 1.6 is the latest
+
+  // TODO what to do with <always_on>? SDFormat docs seem to say if true
+  //  then the update_rate will be obeyed. Gazebo seems to use it as if
+  //  true means enable the sensor at startup.
+
+  // name, always_on, update_rate, visualize, topic, frame, 
+  // pose, plugin
+
+  this->name = _sdf->Get<std::string>("name");
+
+  if (_sdf->HasElement("topic"))
+  {
+    this->topic = _sdf->Get<std::string>("topic");
+  }
+
+  if (_sdf->HasElement("pose"))
+  {
+    this->pose = _sdf->Get<ignition::math::Pose3d>("pose");
+  }
+
+  if (_sdf->HasElement("update_rate"))
+  {
+    this->updateRate = (_sdf->Get<double>("update_rate"));
+  }
+
+  // Get info about plugins
+  if (_sdf->HasElement("plugin"))
+  {
+    sdf::ElementPtr pluginElem = _sdf->GetElement("plugin");
+    while (pluginElem)
+    {
+      PluginDescription desc;
+      desc.pluginName = pluginElem->Get<std::string>("name");
+      desc.pluginFileName = pluginElem->Get<std::string>("filename");
+      this->plugins.push_back(desc);
+      pluginElem = pluginElem->GetNextElement("plugin");
+    }
+  }
+}
 
 
 //////////////////////////////////////////////////
@@ -48,7 +117,9 @@ Sensor::~Sensor()
 //////////////////////////////////////////////////
 void Sensor::Load(sdf::ElementPtr _sdf)
 {
-  // TODO load basic sensor parameters
+  this->dataPtr->PopulateFromSDF(_sdf);
+
+  // Load a plugin if applicable
 }
 
 //////////////////////////////////////////////////
