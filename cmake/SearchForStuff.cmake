dee7f14d9b2ce08cb28bcba2ab5f1576c7d047d1
Shane Loretz <sloretz@osrfoundation.org>
2017-09-14 11:28:33 -0700

    Narrowed where dependencies are used

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -21,9 +21,6 @@ if (NOT ignition-math4_FOUND)
   BUILD_ERROR ("Missing: Ignition math (libignition-math4-dev)")
 else()
   message (STATUS "Found Ignition Math")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-MATH_CXX_FLAGS}")
-  include_directories(${IGNITION-MATH_INCLUDE_DIRS})
-  link_directories(${IGNITION-MATH_LIBRARY_DIRS})
 endif()
 
 ################################################################################
@@ -33,9 +30,6 @@ if (NOT ignition-transport3_FOUND)
   BUILD_ERROR ("Missing: Ignition transport (libignition-transport3-dev)")
 else()
   message (STATUS "Found Ignition transport")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-TRANSPORT_CXX_FLAGS}")
-  include_directories(${IGNITION-TRANSPORT_INCLUDE_DIRS})
-  link_directories(${IGNITION-TRANSPORT_LIBRARY_DIRS})
 endif()
 
 ################################################################################
@@ -54,9 +48,6 @@ if (NOT ignition-common0_FOUND)
   BUILD_ERROR ("Missing: Ignition Common (libignition-common0-dev)")
 else()
   message (STATUS "Found Ignition Common")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${IGNITION-COMMON_CXX_FLAGS}")
-  include_directories(${IGNITION-COMMON_INCLUDE_DIRS})
-  link_directories(${IGNITION-COMMON_LIBRARY_DIRS})
 endif()
 
 ################################################################################
