cda2e5bfcaa33756e716b042ff791d62eb568cde
Ian Chen <ichen@osrfoundation.org>
2019-02-13 18:29:27 -0800

    add sensor factory class

---
+++ src/SensorFactory.cc
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <ignition/common/PluginLoader.hh>
+#include <ignition/common/SystemPaths.hh>
+#include <ignition/common/Console.hh>
+#include "ignition/sensors/config.hh"
+
+#include "ignition/sensors/SensorFactory.hh"
+
+/// \brief Private data class for SensorFactory
+class ignition::sensors::SensorFactoryPrivate
+{
+  /// \brief Constructor
+  public: SensorFactoryPrivate();
+
+  /// \brief A map of loaded sensors and their id's.
+  public: std::map<SensorId, std::shared_ptr<Sensor>> sensors;
+
+  /// \brief Stores paths to search for on file system
+  public: ignition::common::SystemPaths systemPaths;
+
+  /// \brief For loading plugins
+  public: ignition::common::PluginLoader pluginLoader;
+};
+
+using namespace ignition;
+using namespace sensors;
+
+//////////////////////////////////////////////////
+SensorFactoryPrivate::SensorFactoryPrivate()
+{
+  this->systemPaths.AddPluginPaths(IGN_SENSORS_PLUGIN_PATH);
+}
+
+//////////////////////////////////////////////////
+void SensorFactory::AddPluginPaths(const std::string &_path)
+{
+  this->dataPtr->systemPaths.AddPluginPaths(_path);
+}
+
+//////////////////////////////////////////////////
+SensorFactory::SensorFactory() : dataPtr(new SensorFactoryPrivate)
+{
+}
+
+//////////////////////////////////////////////////
+SensorFactory::~SensorFactory()
+{
+}
+
+//////////////////////////////////////////////////
+Sensor *SensorFactory::LoadSensorPlugin(
+    const std::string &_filename, sdf::ElementPtr _sdf)
+{
+  std::string fullPath =
+      this->dataPtr->systemPaths.FindSharedLibrary(_filename);
+  if (fullPath.empty())
+  {
+    ignerr << "Unable to find sensor plugin path for [" << _filename << "]\n";
+    return nullptr;
+  }
+
+  auto pluginNames = this->dataPtr->pluginLoader.LoadLibrary(fullPath);
+  if (pluginNames.empty())
+  {
+    ignerr << "Unable to load sensor plugin file for [" << fullPath << "]\n";
+    return nullptr;
+  }
+
+  // Assume the first plugin is the one we're interested in
+  std::string pluginName = *(pluginNames.begin());
+
+  common::PluginPtr pluginPtr =
+      this->dataPtr->pluginLoader.Instantiate(pluginName);
+
+  auto sensor = pluginPtr->QueryInterfaceSharedPtr<ignition::sensors::Sensor>();
+  if (!sensor)
+  {
+    ignerr << "Unable to instantiate sensor plugin for [" << fullPath << "]\n";
+    return nullptr;
+  }
+
+  if (!sensor->Load(_sdf))
+  {
+    ignerr << "Sensor::Load failed for plugin [" << fullPath << "]\n";
+    return nullptr;
+  }
+
+  if (!sensor->Init())
+  {
+    ignerr << "Sensor::Init failed for plugin [" << fullPath << "]\n";
+    return nullptr;
+  }
+
+  this->dataPtr->sensors[sensor->Id()] = sensor;
+
+  return sensor.get();
+}
+
+/////////////////////////////////////////////////
+Sensor *SensorFactory::CreateSensor(sdf::ElementPtr _sdf)
+{
+  if (_sdf)
+  {
+    if (_sdf->GetName() == "sensor")
+    {
+      std::string type = _sdf->Get<std::string>("type");
+      return this->LoadSensorPlugin(
+          IGN_SENSORS_PLUGIN_NAME(type), _sdf);
+    }
+    else
+    {
+      ignerr << "Provided SDF is not a <sensor> element.\n";
+    }
+  }
+
+  return nullptr;
+}
