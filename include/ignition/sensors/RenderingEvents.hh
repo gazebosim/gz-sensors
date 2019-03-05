68427bb542fa7d9e847c267b37ef6935293696d8
Ian Chen <ichen@osrfoundation.org>
2019-03-05 15:56:04 -0800

    add headers

---
+++ include/ignition/sensors/RenderingEvents.hh
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef IGNITION_SENSORS_RENDERINGEVENTS_HH_
+#define IGNITION_SENSORS_RENDERINGEVENTS_HH_
+
+#include <ignition/common/Event.hh>
+#include <ignition/rendering/Scene.hh>
+#include <ignition/sensors/config.hh>
+#include <ignition/sensors/rendering_sensor/Export.hh>
+
+namespace ignition
+{
+  namespace sensors
+  {
+    // Inline bracket to help doxygen filtering.
+    inline namespace IGNITION_SENSORS_VERSION_NAMESPACE {
+    //
+    class IGNITION_SENSORS_RENDERING_SENSOR_VISIBLE RenderingEvents
+    {
+      /// \brief Set a callback to be called when the scene is changed.
+      ///
+      /// \param[in] _callback  This callback will be called every time the
+      /// scene is changed.
+      /// \remark Do not block inside of the callback.
+      /// \return A connection pointer that must remain in scope. When the
+      /// connection pointer falls out of scope, the connection is broken.
+      public: static ignition::common::ConnectionPtr ConnectSceneChangeCallback(
+                  std::function<void(const ignition::rendering::ScenePtr &)>
+                  _callback);
+
+      /// \brief Event that is used to trigger callbacks when the scene
+      /// is changed
+      public: static ignition::common::EventT<
+              void(const ignition::rendering::ScenePtr &)> sceneEvent;
+    };
+    }
+  }
+}
+#endif
