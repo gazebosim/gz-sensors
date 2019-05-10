3c20f836ecf0f0189502715120453423c568d4fc
Carlos Agüero <caguero@osrfoundation.org>
2019-05-10 15:25:29 -0700

    Updated comment.

--- src/CameraSensor.cc
+++ src/CameraSensor.cc
@@ -325,8 +325,8 @@ bool CameraSensor::Update(const ignition::common::Time &_now)
   msg.set_height(height);
   msg.set_step(width * rendering::PixelUtil::BytesPerPixel(
                this->dataPtr->camera->ImageFormat()));
-  // TODO(anyone) This should be deprecated in the next version of
-  // msgs/sensors in favor of set_pixel_format_type.
+  // TODO(anyone) Deprecated in ign-msgs4, will be removed on ign-msgs5
+  // in favor of set_pixel_format_type.
   msg.set_pixel_format(format);
   msg.set_pixel_format_type(msgsPixelFormat);
   msg.mutable_header()->mutable_stamp()->set_sec(_now.sec);
