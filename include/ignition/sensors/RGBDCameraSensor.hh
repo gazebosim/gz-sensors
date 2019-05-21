5316c778a1a6ca4113b4c0e2f8da1b9882640da3
Nate Koenig <natekoenig@gmail.com>
2019-05-20 21:30:36 -0700

    Adding an RGBD sensor

---
+++ include/ignition/sensors/RGBDCameraSensor.hh
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef IGNITION_SENSORS_RGBDCAMERASENSOR_HH_
+#define IGNITION_SENSORS_RGBDCAMERASENSOR_HH_
+
+#include <memory>
+#include <cstdint>
+#include <mutex>
+#include <string>
+
+#include <sdf/Sensor.hh>
+
+#include <ignition/msgs/image.pb.h>
+
+#include <ignition/sensors/Export.hh>
+#include <ignition/sensors/Sensor.hh>
+#include <ignition/sensors/RenderingSensor.hh>
+#include <ignition/transport.hh>
+
+
+#ifndef _WIN32
+#  define RGBDCameraSensor_EXPORTS_API
+#else
+#  if (defined(RGBDCameraSensor_EXPORTS))
+#    define RGBDCameraSensor_EXPORTS_API __declspec(dllexport)
+#  else
+#    define RGBDCameraSensor_EXPORTS_API __declspec(dllimport)
+#  endif
+#endif
+
+namespace ignition
+{
+  namespace sensors
+  {
+    // Inline bracket to help doxygen filtering.
+    inline namespace IGNITION_SENSORS_VERSION_NAMESPACE {
+    // forward declarations
+    class RGBDCameraSensorPrivate;
+
+    /// \brief Depth camera sensor class.
+    ///
+    /// This class creates depth image from an ignition rendering scene.
+    /// The scene  must be created in advance and given to Manager::Init().
+    /// It offers both an ignition-transport interface and a direct C++ API
+    /// to access the image data. The API works by setting a callback to be
+    /// called with image data.
+    class RGBDCameraSensor_EXPORTS_API RGBDCameraSensor : public RenderingSensor
+    {
+      /// \brief constructor
+      public: RGBDCameraSensor();
+
+      /// \brief destructor
+      public: virtual ~RGBDCameraSensor();
+
+      /// \brief Load the sensor based on data from an sdf::Sensor object.
+      /// \param[in] _sdf SDF Sensor parameters.
+      /// \return true if loading was successful
+      public: virtual bool Load(const sdf::Sensor &_sdf) override;
+
+      /// \brief Initialize values in the sensor
+      /// \return True on success
+      public: virtual bool Init() override;
+
+      /// \brief Force the sensor to generate data
+      /// \param[in] _now The current time
+      /// \return true if the update was successfull
+      public: virtual bool Update(const common::Time &_now) override;
+
+      /// \brief Get a pointer to the depth camera
+      /// \return Pointer to the depth camera
+      public: rendering::DepthCameraPtr DepthCamera();
+
+      /// \brief Get a pointer to the 2d image camera
+      /// \return Pointer to the 2d image camera.
+      public: rendering::CameraPtr RenderingCamera();
+
+      /// \brief Set the rendering scene.
+      /// \param[in] _scene Pointer to the scene
+      public: virtual void SetScene(
+                  ignition::rendering::ScenePtr _scene) override;
+
+      /// \brief Get image width.
+      /// \return width of the image
+      public: unsigned int ImageWidth() const;
+
+      /// \brief Get image height.
+      /// \return height of the image
+      public: unsigned int ImageHeight() const;
+
+      /// \brief Get image width.
+      /// \return width of the image
+      public: virtual double FarClip() const;
+
+      /// \brief Get image height.
+      /// \return height of the image
+      public: virtual double NearClip() const;
+
+      /// \brief Data pointer for private data
+      /// \internal
+      private: std::unique_ptr<RGBDCameraSensorPrivate> dataPtr;
+    };
+    }
+  }
+}
+
+#endif
---
+++ src/RGBDCameraSensor.cc
@@ -0,0 +1,459 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <ignition/msgs/pointcloud2.pb.h>
+#include <ignition/msgs/image.pb.h>
+#include <ignition/math/Helpers.hh>
+
+#include <ignition/rendering/Camera.hh>
+#include <ignition/rendering/DepthCamera.hh>
+
+#include <sdf/Sensor.hh>
+
+#include "ignition/sensors/RGBDCameraSensor.hh"
+#include "ignition/sensors/SensorFactory.hh"
+#include "ignition/sensors/GaussianNoiseModel.hh"
+
+/// \brief Private data for RGBDCameraSensor
+class ignition::sensors::RGBDCameraSensorPrivate
+{
+  /// \brief Create the cameras in a scene.
+  /// \return True on success.
+  public: bool CreateCameras(const std::string &_name,
+              ignition::rendering::ScenePtr _scene);
+
+
+  /// \brief Remove a camera from a scene
+  public: void RemoveCamera(ignition::rendering::ScenePtr _scene);
+
+  /// \brief node to create publisher
+  public: transport::Node node;
+
+  /// \brief publisher to publish images
+  public: transport::Node::Publisher imagePub;
+
+  /// \brief publisher to publish depth images
+  public: transport::Node::Publisher depthPub;
+
+  /// \brief publisher to publish points clouds
+  public: transport::Node::Publisher pointPub;
+
+  /// \brief Camera info publisher to publish images
+  public: transport::Node::Publisher infoPub;
+
+  /// \brief true if Load() has been called and was successful
+  public: bool initialized = false;
+
+  /// \brief Rendering camera
+  public: ignition::rendering::CameraPtr camera;
+
+  /// \brief Rendering camera
+  public: ignition::rendering::DepthCameraPtr depthCamera;
+
+  /// \brief Depth data buffer.
+  public: float *depthBuffer = nullptr;
+
+  /// \brief Pointer to an image to be published
+  public: ignition::rendering::Image image;
+
+  /// \brief Pointer to a depth image to be published
+  public: ignition::rendering::Image depthImage;
+
+  /// \brief Noise added to sensor data
+  public: std::map<SensorNoiseType, NoisePtr> noises;
+
+  /// \brief Event that is used to trigger callbacks when a new image
+  /// is generated
+  public: ignition::common::EventT<
+          void(const ignition::msgs::Image &)> imageEvent;
+
+  /// \brief Connection from depth camera with a new image
+  public: ignition::common::ConnectionPtr connection;
+
+  /// \brief Connection to the Manager's scene change event.
+  public: ignition::common::ConnectionPtr sceneChangeConnection;
+
+  /// \brief Just a mutex for thread safety
+  public: std::mutex mutex;
+
+  /// \brief True to save images
+  public: bool saveImage = false;
+
+  /// \brief path directory to where images are saved
+  public: std::string saveImagePath = "./";
+
+  /// \prefix of an image name
+  public: std::string saveImagePrefix = "./";
+
+  /// \brief counter used to set the image filename
+  public: std::uint64_t saveImageCounter = 0;
+
+  /// \brief SDF Sensor DOM object.
+  public: sdf::Sensor sdfSensor;
+
+  /// \brief Camera information message.
+  public: msgs::CameraInfo infoMsg;
+};
+
+using namespace ignition;
+using namespace sensors;
+
+//////////////////////////////////////////////////
+void RGBDCameraSensorPrivate::RemoveCamera(
+    rendering::ScenePtr _scene)
+{
+  if (_scene)
+  {
+    // \todo(nkoenig) Remove camera from scene!
+  }
+  // this->depthCamera = nullptr;
+}
+
+
+//////////////////////////////////////////////////
+RGBDCameraSensor::RGBDCameraSensor()
+  : dataPtr(new RGBDCameraSensorPrivate())
+{
+}
+
+//////////////////////////////////////////////////
+RGBDCameraSensor::~RGBDCameraSensor()
+{
+  this->dataPtr->connection.reset();
+  if (this->dataPtr->depthBuffer)
+    delete [] this->dataPtr->depthBuffer;
+}
+
+//////////////////////////////////////////////////
+bool RGBDCameraSensor::Init()
+{
+  return this->Sensor::Init();
+}
+
+//////////////////////////////////////////////////
+bool RGBDCameraSensor::Load(const sdf::Sensor &_sdf)
+{
+  std::lock_guard<std::mutex> lock(this->dataPtr->mutex);
+
+  if (!Sensor::Load(_sdf))
+  {
+    return false;
+  }
+
+  // Check if this is the right type
+  if (_sdf.Type() != sdf::SensorType::RGBD_CAMERA)
+  {
+    ignerr << "Attempting to a load a RGBD Camera sensor, but received "
+      << "a " << _sdf.TypeStr() << std::endl;
+  }
+
+  if (_sdf.CameraSensor() == nullptr)
+  {
+    ignerr << "Attempting to a load an RGBD Camera sensor, but received "
+      << "a null sensor." << std::endl;
+    return false;
+  }
+
+  this->dataPtr->sdfSensor = _sdf;
+
+  // Create the 2d image publisher
+  this->dataPtr->imagePub =
+      this->dataPtr->node.Advertise<ignition::msgs::Image>(
+          this->Topic() + "/image");
+  if (!this->dataPtr->imagePub)
+    return false;
+
+  // Create the depth image publisher
+  this->dataPtr->depthPub =
+      this->dataPtr->node.Advertise<ignition::msgs::Image>(
+          this->Topic() + "/depth_image");
+  if (!this->dataPtr->depthPub)
+    return false;
+
+  // Create the point cloud publisher
+  this->dataPtr->pointPub =
+      this->dataPtr->node.Advertise<ignition::msgs::PointCloud2>(
+          this->Topic() + "/points");
+  if (!this->dataPtr->pointPub)
+    return false;
+
+  this->dataPtr->infoPub =
+      this->dataPtr->node.Advertise<ignition::msgs::CameraInfo>(
+          this->Topic() + "/camera_info");
+  if (!this->dataPtr->infoPub)
+    return false;
+
+  if (this->Scene())
+    this->dataPtr->CreateCameras(this->Name(), this->Scene());
+
+  this->dataPtr->initialized = true;
+
+  return true;
+}
+
+//////////////////////////////////////////////////
+bool RGBDCameraSensorPrivate::CreateCameras(const std::string &_name,
+              ignition::rendering::ScenePtr _scene)
+{
+  const sdf::Camera *cameraSdf = this->sdfSensor.CameraSensor();
+
+  if (!cameraSdf)
+  {
+    ignerr << "Unable to access camera SDF element\n";
+    return false;
+  }
+
+  int width = cameraSdf->ImageWidth();
+  int height = cameraSdf->ImageHeight();
+
+  // Set some values of the camera info message.
+  {
+    msgs::CameraInfo::Distortion *distortion =
+      this->infoMsg.mutable_distortion();
+
+    distortion->set_distortion_model(msgs::CameraInfo::Distortion::PLUMB_BOB);
+    distortion->set_k1(cameraSdf->DistortionK1());
+    distortion->set_k2(cameraSdf->DistortionK2());
+    distortion->set_k3(cameraSdf->DistortionK3());
+    distortion->set_t1(cameraSdf->DistortionP1());
+    distortion->set_t2(cameraSdf->DistortionP2());
+
+    msgs::CameraInfo::Intrinsics *intrinsics =
+      this->infoMsg.mutable_intrinsics();
+    intrinsics->set_fx(cameraSdf->LensIntrinsicsFx());
+    intrinsics->set_fy(cameraSdf->LensIntrinsicsFy());
+    intrinsics->set_cx(cameraSdf->LensIntrinsicsCx());
+    intrinsics->set_cy(cameraSdf->LensIntrinsicsCy());
+
+    msgs::CameraInfo::Projection *proj =
+      this->infoMsg.mutable_projection();
+    proj->set_fx(cameraSdf->LensIntrinsicsFx());
+    proj->set_fy(cameraSdf->LensIntrinsicsFy());
+    proj->set_cx(cameraSdf->LensIntrinsicsCx());
+    proj->set_cy(cameraSdf->LensIntrinsicsCy());
+
+    // Set the rectifcation matrix to identity
+    this->infoMsg.add_rectification_matrix(1.0);
+    this->infoMsg.add_rectification_matrix(0.0);
+    this->infoMsg.add_rectification_matrix(0.0);
+
+    this->infoMsg.add_rectification_matrix(0.0);
+    this->infoMsg.add_rectification_matrix(1.0);
+    this->infoMsg.add_rectification_matrix(0.0);
+
+    this->infoMsg.add_rectification_matrix(0.0);
+    this->infoMsg.add_rectification_matrix(0.0);
+    this->infoMsg.add_rectification_matrix(1.0);
+
+    auto infoFrame = this->infoMsg.mutable_header()->add_data();
+    infoFrame->set_key("frame_id");
+    infoFrame->add_value(_name);
+
+    this->infoMsg.set_width(width);
+    this->infoMsg.set_height(height);
+  }
+
+  this->depthCamera = _scene->CreateDepthCamera(_name + "_depth");
+  this->depthCamera->SetImageWidth(width);
+  this->depthCamera->SetImageHeight(height);
+  this->depthCamera->SetFarClipPlane(cameraSdf->FarClip());
+
+  this->camera = _scene->CreateCamera(_name);
+  this->camera->SetImageWidth(width);
+  this->camera->SetImageHeight(height);
+  this->camera->SetNearClipPlane(cameraSdf->NearClip());
+  this->camera->SetFarClipPlane(cameraSdf->FarClip());
+
+  /*const std::map<SensorNoiseType, sdf::Noise> noises = {
+    {CAMERA_NOISE, cameraSdf->ImageNoise()},
+  };
+
+  for (const auto & [noiseType, noiseSdf] : noises)
+  {
+    // Add gaussian noise to camera sensor
+    if (noiseSdf.Type() == sdf::NoiseType::GAUSSIAN)
+    {
+      this->noises[noiseType] =
+        NoiseFactory::NewNoiseModel(noiseSdf, "depth");
+
+      std::dynamic_pointer_cast<ImageGaussianNoiseModel>(
+           this->noises[noiseType])->SetCamera(
+             this->depthCamera);
+    }
+    else if (noiseSdf.Type() != sdf::NoiseType::NONE)
+    {
+      ignwarn << "The depth camera sensor only supports Gaussian noise. "
+       << "The supplied noise type[" << static_cast<int>(noiseSdf.Type())
+       << "] is not supported." << std::endl;
+    }
+  }
+  */
+
+  // \todo(nkoeng) these parameters via sdf
+  this->depthCamera->SetAntiAliasing(2);
+  this->camera->SetAntiAliasing(2);
+
+  math::Angle angle = cameraSdf->HorizontalFov();
+  if (angle < 0.01 || angle > IGN_PI*2)
+  {
+    ignerr << "Invalid horizontal field of view [" << angle << "]\n";
+
+    return false;
+  }
+
+  this->depthCamera->SetAspectRatio(static_cast<double>(width)/height);
+  this->depthCamera->SetHFOV(angle);
+  this->camera->SetAspectRatio(static_cast<double>(width)/height);
+  this->camera->SetHFOV(angle);
+
+  // Create depth texture when the camera is reconfigured from default values
+  this->depthCamera->CreateDepthTexture();
+
+  // \todo(nkoenig) Port Distortion class
+  // This->distortion.reset(new Distortion());
+  // This->distortion->Load(this->sdf->GetElement("distortion"));
+
+  this->depthCamera->SetImageFormat(ignition::rendering::PF_FLOAT32_R);
+  this->depthImage = this->depthCamera->CreateImage();
+
+  this->camera->SetImageFormat(ignition::rendering::PF_R8G8B8);
+  this->image = this->depthCamera->CreateImage();
+
+  _scene->RootVisual()->AddChild(this->depthCamera);
+  _scene->RootVisual()->AddChild(this->camera);
+
+  return true;
+}
+
+/////////////////////////////////////////////////
+void RGBDCameraSensor::SetScene(ignition::rendering::ScenePtr _scene)
+{
+  std::lock_guard<std::mutex> lock(this->dataPtr->mutex);
+  // APIs make it possible for the scene pointer to change
+  if (this->Scene() != _scene)
+  {
+    this->dataPtr->RemoveCamera(this->Scene());
+    RenderingSensor::SetScene(_scene);
+
+    if (this->dataPtr->initialized)
+      this->dataPtr->CreateCameras(this->Name(), _scene);
+  }
+}
+
+//////////////////////////////////////////////////
+bool RGBDCameraSensor::Update(const ignition::common::Time &_now)
+{
+  if (!this->dataPtr->initialized)
+  {
+    ignerr << "Not initialized, update ignored.\n";
+    return false;
+  }
+
+  if (!this->dataPtr->depthCamera || !this->dataPtr->camera)
+  {
+    ignerr << "Depth or image cameras do exist.\n";
+    return false;
+  }
+
+  // move the camera to the current pose
+  this->dataPtr->camera->SetLocalPose(this->Pose());
+  this->dataPtr->depthCamera->SetLocalPose(this->Pose());
+
+  unsigned int width = this->dataPtr->camera->ImageWidth();
+  unsigned int height = this->dataPtr->camera->ImageHeight();
+
+  // create and publish the 2d image message
+  {
+    this->dataPtr->camera->Capture(this->dataPtr->image);
+    unsigned char *data = this->dataPtr->image.Data<unsigned char>();
+
+    ignition::msgs::Image msg;
+    msg.set_width(width);
+    msg.set_height(height);
+    msg.set_step(width * rendering::PixelUtil::BytesPerPixel(
+          this->dataPtr->camera->ImageFormat()));
+    msg.set_pixel_format_type(msgs::PixelFormatType::RGB_INT8);
+    msg.mutable_header()->mutable_stamp()->set_sec(_now.sec);
+    msg.mutable_header()->mutable_stamp()->set_nsec(_now.nsec);
+    auto frame = msg.mutable_header()->add_data();
+    frame->set_key("frame_id");
+    frame->add_value(this->Name());
+    msg.set_data(data, this->dataPtr->camera->ImageMemorySize());
+
+    // publish the image message
+    this->dataPtr->imagePub.Publish(msg);
+  }
+
+  // create and publish the depthmessage
+  {
+    // generate sensor data
+    this->dataPtr->depthCamera->Update();
+
+    ignition::msgs::Image msg;
+    msg.set_width(width);
+    msg.set_height(height);
+    msg.set_step(width * rendering::PixelUtil::BytesPerPixel(
+          this->dataPtr->depthCamera->ImageFormat()));
+    msg.set_pixel_format_type(msgs::PixelFormatType::R_FLOAT32);
+    msg.mutable_header()->mutable_stamp()->set_sec(_now.sec);
+    msg.mutable_header()->mutable_stamp()->set_nsec(_now.nsec);
+    auto frame = msg.mutable_header()->add_data();
+    frame->set_key("frame_id");
+    frame->add_value(this->Name());
+
+    std::lock_guard<std::mutex> lock(this->dataPtr->mutex);
+    msg.set_data(this->dataPtr->depthBuffer,
+        this->dataPtr->depthCamera->ImageMemorySize());
+
+    // publish
+    this->dataPtr->depthPub.Publish(msg);
+  }
+
+  // publish the camera info message
+  this->dataPtr->infoMsg.mutable_header()->mutable_stamp()->set_sec(_now.sec);
+  this->dataPtr->infoMsg.mutable_header()->mutable_stamp()->set_nsec(
+      _now.nsec);
+  this->dataPtr->infoPub.Publish(this->dataPtr->infoMsg);
+
+  return true;
+}
+
+//////////////////////////////////////////////////
+unsigned int RGBDCameraSensor::ImageWidth() const
+{
+  return this->dataPtr->depthCamera->ImageWidth();
+}
+
+//////////////////////////////////////////////////
+unsigned int RGBDCameraSensor::ImageHeight() const
+{
+  return this->dataPtr->depthCamera->ImageHeight();
+}
+
+//////////////////////////////////////////////////
+double RGBDCameraSensor::FarClip() const
+{
+  return this->dataPtr->depthCamera->FarClipPlane();
+}
+
+//////////////////////////////////////////////////
+double RGBDCameraSensor::NearClip() const
+{
+  this->dataPtr->sdfSensor.CameraSensor()->NearClip();
+}
+
+IGN_SENSORS_REGISTER_SENSOR(RGBDCameraSensor)
