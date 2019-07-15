1d49f9df1027df0ff0c9d9529f3ec99a2a6ab726
Nate Koenig <natekoenig@gmail.com>
2019-07-14 22:00:55 -0700

    Working on GPU lidar pointclouds

--- src/GpuLidarSensor.cc
+++ src/GpuLidarSensor.cc
@@ -14,20 +14,37 @@
  * limitations under the License.
  *
 */
+#include <ignition/msgs/pointcloud_packed.pb.h>
+
 #include <ignition/common/Console.hh>
 #include "ignition/sensors/GpuLidarSensor.hh"
 #include "ignition/sensors/SensorFactory.hh"
+
+#include "PointCloud.hh"
 
 using namespace ignition::sensors;
 
 /// \brief Private data for the GpuLidar class
 class ignition::sensors::GpuLidarSensorPrivate
 {
+  public: void FillMsg();
+
   /// \brief Rendering camera
   public: ignition::rendering::GpuRaysPtr gpuRays;
 
   /// \brief Connection to the Manager's scene change event.
   public: ignition::common::ConnectionPtr sceneChangeConnection;
+
+  /// \brief The point cloud message.
+  public: msgs::PointCloudPacked pointMsg;
+
+  /// \brief node to create publisher
+  public: transport::Node node;
+
+  /// \brief publisher to publish point cloud
+  public: transport::Node::Publisher pointPub;
+
+  public: PointCloud pointCloud;
 };
 
 //////////////////////////////////////////////////
@@ -93,7 +110,46 @@ bool GpuLidarSensor::Load(const sdf::Sensor &_sdf)
     RenderingEvents::ConnectSceneChangeCallback(
         std::bind(&GpuLidarSensor::SetScene, this, std::placeholders::_1));
 
+  // Create the depth image publisher
+  this->dataPtr->pointPub =
+      this->dataPtr->node.Advertise<ignition::msgs::PointCloudPacked>(
+          this->Topic() + "/points");
+  if (!this->dataPtr->pointPub)
+    return false;
+
+
   this->initialized = true;
+
+  // Setup the point cloud message.
+  uint32_t offset = 0;
+  msgs::PointCloudPacked::Field *field = this->dataPtr->pointMsg.add_field();
+  field->set_name("x");
+  field->set_count(1);
+  field->set_datatype(msgs::PointCloudPacked::Field::FLOAT32);
+  field->set_offset(offset);
+  offset += 4;
+
+  field = this->dataPtr->pointMsg.add_field();
+  field->set_name("y");
+  field->set_count(1);
+  field->set_datatype(msgs::PointCloudPacked::Field::FLOAT32);
+  field->set_offset(offset);
+  offset += 4;
+
+  field = this->dataPtr->pointMsg.add_field();
+  field->set_name("z");
+  field->set_count(1);
+  field->set_datatype(msgs::PointCloudPacked::Field::FLOAT32);
+  field->set_offset(offset);
+  offset += 4;
+
+  this->dataPtr->pointMsg.set_point_step(offset);
+
+  // Set the frame
+  msgs::Header::Map *frame =
+    this->dataPtr->pointMsg.mutable_header()->add_data();
+  frame->set_key("frame_id");
+  frame->add_value(this->Name());
 
   return true;
 }
@@ -179,6 +235,25 @@ bool GpuLidarSensor::Update(const ignition::common::Time &_now)
 
   this->PublishLidarScan(_now);
 
+  {
+    // Set the time stamp
+    this->dataPtr->pointMsg.mutable_header()->mutable_stamp()->set_sec(
+        _now.sec);
+    this->dataPtr->pointMsg.mutable_header()->mutable_stamp()->set_nsec(
+        _now.nsec);
+    this->dataPtr->pointMsg.set_width(this->dataPtr->gpuRays->ImageWidth());
+    this->dataPtr->pointMsg.set_height(this->dataPtr->gpuRays->ImageHeight());
+    this->dataPtr->pointMsg.set_row_step(
+        this->dataPtr->pointMsg.point_step() *
+        this->dataPtr->gpuRays->ImageWidth() *
+        this->dataPtr->gpuRays->ImageHeight());
+
+    this->dataPtr->pointMsg.set_is_dense(true);
+
+    this->dataPtr->FillMsg();
+
+    this->dataPtr->pointPub.Publish(this->dataPtr->pointMsg);
+  }
   return true;
 }
 
@@ -213,6 +288,57 @@ ignition::math::Angle GpuLidarSensor::HFOV() const
 ignition::math::Angle GpuLidarSensor::VFOV() const
 {
   return this->dataPtr->gpuRays->VFOV();
+}
+
+//////////////////////////////////////////////////
+void GpuLidarSensorPrivate::FillMsg()
+{
+  uint32_t width = this->pointMsg.width();
+  uint32_t height = this->pointMsg.height();
+
+  double angleStep =
+    (this->gpuRays->AngleMax() - this->gpuRays->AngleMin()).Radian() /
+    (this->gpuRays->RangeCount()-1);
+
+  double verticleAngleStep = (this->gpuRays->VerticalAngleMax() -
+      this->gpuRays->VerticalAngleMin()).Radian() /
+    (this->gpuRays->VerticalRangeCount()-1);
+
+  // Angles of ray currently processing, azimuth is horizontal, inclination
+  // is vertical
+  double inclination = this->gpuRays->VerticalAngleMin().Radian();
+  int channels = 3;
+
+  std::string *msgBuffer = this->pointMsg.mutable_data();
+  msgBuffer->resize(this->pointMsg.row_step());
+  char *msgBufferIndex = msgBuffer->data();
+
+  // Iterate over scan and populate point cloud
+  for (uint32_t j = 0; j < height; ++j)
+  {
+    double azimuth = this->gpuRays->AngleMin().Radian();
+
+    for (uint32_t i = 0; i < width; ++i)
+    {
+      // Index of current point
+      auto index = j * width * channels + i * channels;
+      double depth = this->gpuRays->Data()[index];
+
+      // Convert spherical coordinates to Cartesian for pointcloud
+      // See https://en.wikipedia.org/wiki/Spherical_coordinate_system
+      *reinterpret_cast<float*>(msgBufferIndex) =
+        depth * std::cos(inclination) * std::cos(azimuth);
+      msgBufferIndex += 4;
+      *reinterpret_cast<float*>(msgBufferIndex) =
+        depth * std::cos(inclination) * std::sin(azimuth);
+      msgBufferIndex += 4;
+      *reinterpret_cast<float*>(msgBufferIndex) = depth * std::sin(inclination);
+      msgBufferIndex += 4;
+
+      azimuth += angleStep;
+    }
+    inclination += verticleAngleStep;
+  }
 }
 
 IGN_SENSORS_REGISTER_SENSOR(GpuLidarSensor)
