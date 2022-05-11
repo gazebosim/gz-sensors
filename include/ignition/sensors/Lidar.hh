387b354c7aa365ac716dd97ef06058f510e2234d
Ian Chen <ichen@osrfoundation.org>
2022-05-11 14:44:20 -0700

    sets lidar visibility mask
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- include/ignition/sensors/Lidar.hh
+++ include/ignition/sensors/Lidar.hh
@@ -235,6 +235,9 @@ namespace ignition
 
       // Documentation inherited
       public: virtual bool IsActive() const;
+      /// \brief Get the visibility mask
+      /// \return Visibility mask
+      public: uint32_t VisibilityMask() const;
 
       IGN_COMMON_WARN_IGNORE__DLL_INTERFACE_MISSING
       /// \brief Just a mutex for thread safety
