9794634581e64597e25bc3a81411e141062e6909
methylDragon <methylDragon@gmail.com>
2022-05-18 02:28:26 -0700

    Implement deprecation trick
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- include/ignition/sensors/config.hh
+++ include/ignition/sensors/config.hh
@@ -15,4 +15,14 @@
  *
  */
 
+#ifndef IGNITION_SENSORS__CONFIG_HH_
+#define IGNITION_SENSORS__CONFIG_HH_
+
 #include <gz/sensors/config.hh>
+
+namespace [[deprecated("Use gz namespace.")]]  ignition
+{
+  using namespace gz;
+}
+
+#endif
