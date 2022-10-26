adeb66057ce156b2690f2b470716a9ae911a61ec
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-10-26 22:47:24 +0200

    Improved coverage Lidar (#277)
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    Co-authored-by: Michael Carroll <michael@openrobotics.org>

--- src/PointCloudUtil.hh
+++ src/PointCloudUtil.hh
@@ -18,14 +18,14 @@
 #ifndef IGNITION_SENSORS_POINTCLOUDUTIL_HH_
 #define IGNITION_SENSORS_POINTCLOUDUTIL_HH_
 
-#ifdef _WIN32
-#pragma warning(push)
-#pragma warning(disable: 4005)
-#pragma warning(disable: 4251)
+#if defined(_MSC_VER)
+  #pragma warning(push)
+  #pragma warning(disable: 4005)
+  #pragma warning(disable: 4251)
 #endif
 #include <ignition/msgs/pointcloud_packed.pb.h>
-#ifdef _WIN32
-#pragma warning(pop)
+#if defined(_MSC_VER)
+  #pragma warning(pop)
 #endif
 #include <ignition/math/Angle.hh>
 
