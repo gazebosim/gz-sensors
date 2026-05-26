51fe4b748498955baea22b933a10d4c73566b23d
Steve Peters <scpeters@intrinsic.ai>
2026-05-26 09:47:34 -0700

    Fix compiler warnings with protobuf 35.0 (#618)
    Signed-off-by: Steve Peters <scpeters@intrinsic.ai>
    (cherry picked from commit 4cd2bb4a4c61ee299e2814161c9cea913e6b5d2b)

--- src/DopplerVelocityLog.cc
+++ src/DopplerVelocityLog.cc
@@ -1554,7 +1554,11 @@ namespace gz
               bottomModeNoiseVariance *
               beamBasisElement * beamBasisElement.transpose();
 
+#if GOOGLE_PROTOBUF_VERSION >= 7035000
+          beamVelocityMessage->mutable_covariance()->resize(9, 0.);
+#else
           beamVelocityMessage->mutable_covariance()->Resize(9, 0.);
+#endif
           std::copy(beamVelocityCovarianceInReferenceFrame.data(),
                     beamVelocityCovarianceInReferenceFrame.data() + 9,
                     beamVelocityMessage->mutable_covariance()->begin());
@@ -1595,7 +1599,11 @@ namespace gz
         velocityMessage->mutable_mean()->set_z(
             velocityMeanInReferenceFrame.z());
 
+#if GOOGLE_PROTOBUF_VERSION >= 7035000
+        velocityMessage->mutable_covariance()->resize(9, 0.);
+#else
         velocityMessage->mutable_covariance()->Resize(9, 0.);
+#endif
         std::copy(velocityCovarianceInReferenceFrame.data(),
                   velocityCovarianceInReferenceFrame.data() + 9,
                   velocityMessage->mutable_covariance()->begin());
@@ -1783,7 +1791,11 @@ namespace gz
             gz::msgs::Convert(beamAxisInReferenceFrame * averageBeamSpeed);
 
         // Use row-major 1D layout for covariance
+#if GOOGLE_PROTOBUF_VERSION >= 7035000
+        beamVelocityMessage->mutable_covariance()->resize(9, 0.);
+#else
         beamVelocityMessage->mutable_covariance()->Resize(9, 0.);
+#endif
         std::copy(beamVelocityCovarianceInReferenceFrame.data(),
                   beamVelocityCovarianceInReferenceFrame.data() + 9,
                   beamVelocityMessage->mutable_covariance()->begin());
@@ -1826,7 +1838,11 @@ namespace gz
         velocityMessage->mutable_mean()->set_z(
             velocityMeanInReferenceFrame.z());
 
+#if GOOGLE_PROTOBUF_VERSION >= 7035000
+        velocityMessage->mutable_covariance()->resize(9, 0.);
+#else
         velocityMessage->mutable_covariance()->Resize(9, 0.);
+#endif
         std::copy(velocityCovarianceInReferenceFrame.data(),
                   velocityCovarianceInReferenceFrame.data() + 9,
                   velocityMessage->mutable_covariance()->begin());
